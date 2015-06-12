4884c977a3e6d36acc3d87dbef569072cb2d80db
hihell <jiu694@gmail.com>
2015-06-12 14:36:28 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+__author__ = 'jiusi'
+
+from distutils.core import setup
+
+setup(
+    name='motionsoundUtils',
+
+    # this must be the same as the name above
+    packages=['motionsoundUtils'],
+
+    version='0.1',
+    description='utils for motion sound prediction',
+    author='jiusi',
+    author_email='jiusi@petchat.io',
+
+    # use the URL to the github repo
+    url='https://github.com/petchat/motionsoundUtils.git',
+
+    # The download_url is a link to a hosted file with your repository's code
+    # download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+    download_url='https://codeload.github.com/petchat/motionsoundUtils/tar.gz/0.1',
+
+    keywords=[],  # arbitrary keywords
+    classifiers=[]
+)
